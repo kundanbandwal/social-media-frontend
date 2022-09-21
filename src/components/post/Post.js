3e8b3427cb81d2cca68be588edbcd1f2965bfc89
kundan <kundanbandwal@gmail.com>
2022-09-21 10:36:53 +0530

    Update Post.js

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,11 +1,13 @@
-import "./post.css"
-import { MoreVert } from "@material-ui/icons"
-// import { Users } from "../../dummyData"
-import { useState,useEffect } from "react"
-import request from "../../axiosConfig"
+import "./post.css";
+import { MoreVert } from "@material-ui/icons";
+// import { Users } from "../../dummyData";//
+import { useState,useEffect } from "react";
+import request from "../../axiosConfig";
+import { format } from "timeago.js";
+import { Link } from "react-router-dom";
 
-function Post(props) {
-    const [like, setLike] = useState(props.post.like)
+function Post({post}) {
+    const [like, setLike] = useState(post.likes.length)
     const [isLiked, setIsLiked] = useState(false)
     const [user, setUser] = useState({})
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
@@ -13,15 +15,15 @@ function Post(props) {
     useEffect(()=>{
         const fetchUser = async() => {
           try {
-            const res = await request.get('/posts')
-          console.log(res)
+            const res = await request.get(`/users/${post.userId}`);
+        //   console.log(res)
           setUser(res.data)
           } catch (error) {
             console.log(error)
           }
         };
         fetchUser();
-    },[])
+    },[post.userId])
 
     function likeHandler() {
         setLike(isLiked ? like - 1 : like + 1);
@@ -32,29 +34,31 @@ function Post(props) {
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
+                        <Link to={`profile/${user.username}`}>
                         <img className="postProfileImage" src= {user.profilePicture || PF+
                         "person/noAvatar.png"} alt=""  />
+                        </Link>
                         <span className="postUsername">
                             {user.username}
                         </span>
-                        <span className="postDate">{props.post.date}</span>
+                        <span className="postDate">{format(post.createdAt)}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">{props.post?.desc}</span>
-                    <img className="postImage" src={PF + props.post.photo} alt="" />
+                    <span className="postText">{post?.desc}</span>
+                    <img className="postImage" src={PF+post.img} alt="" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt="" />
-                        <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt="" />
+                        <img className="likeIcon" src={`${PF}like.png`} onClick={likeHandler} alt="" />
+                        <img className="likeIcon" src={`${PF}heart.png`} onClick={likeHandler} alt="" />
                         <span className="postLikeCounter">{like} people like it</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">{props.post.comment} comments</span>
+                        <span className="postCommentText">{post.comment} comments</span>
                     </div>
                 </div>
             </div>
