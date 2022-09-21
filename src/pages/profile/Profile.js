01432146db460cd06e809378f5c95434f1bb4d32
kundan <kundanbandwal@gmail.com>
2022-09-21 12:47:56 +0530

    Added Profile

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,10 +1,29 @@
-import "./profile.css"
-import Topbar from "../../components/topbar/Topbar"
-import Sidebar from "../../components/sidebar/Sidebar"
-import Feed from "../../components/feed/Feed"
-import Rightbar from "../../components/rightbar/Rightbar"
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import { useState,useEffect } from "react";
+import request from "../../axiosConfig";
+import { useParams } from "react-router";
+
 function Profile() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({})
+  const username = useParams().username
+  
+  useEffect(()=>{
+    const fetchUser = async() => {
+      try {
+        const res = await request.get(`/users?username=${username}`);
+    //   console.log(res)
+      setUser(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    fetchUser();
+},[username])
     return (
         <>
             <Topbar />
@@ -13,17 +32,19 @@ function Profile() {
                 <div className="profileRight">
                     <div className="profileRightTop">
                         <div className="profileCover">
-                            <img className="profileCoverImage" src={`${PF}post/4.jpeg`} alt="" />
-                            <img className="profileUserImage" src={`${PF}person/8.jpeg`} alt="" />
+                         <img className="profileCoverImage" src={user.coverPicture || PF+"person/noCover.png"} alt="" />
+                         <img className="profileUserImage" src={user.profilePicture || PF+"person/noAvatar.png"} alt="" />
                         </div>
                         <div className="profileInfo">
-                            <h4 className="profileInfoName">kundan bandwal</h4>
-                            <span className="profileInfoDesc">Hello my Friends!</span>
+                            <h4 className="profileInfoName">{user.username}</h4>
+                            <span className="profileInfoDesc">{user.desc}</span>
                         </div>
                     </div>
                     <div className="profileRightBottom">
-                        <Feed />
-                        <Rightbar profile/>
+
+                        
+                        <Feed username={username}/>
+                        <Rightbar user={user}/>
                     </div>
                 </div>
             </div>
