210a00c58369c98be00bef3a45ba2499654c7cd8
kundan <kundanbandwal@gmail.com>
2022-09-21 18:09:23 +0530

    Update Login.js

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,34 +1,57 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 import "./login.css";
-import loginCall from "../../apiCalls"
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from "@material-ui/core"
 
 function Login() {
-const email = useRef();
-const password = useRef();
-function handleClick(e){
-  e.preventDefault()
-}
+  const email = useRef();
+  const password = useRef();
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
+  };
+  console.log(user);
   return (
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-            <h3 className="loginLogo">FaceBook</h3>
-            <span className="loginDesc">
-                Dont use FaceBook its very useless.
-            </span>
+          <h3 className="loginLogo">FaceBook</h3>
+          <span className="loginDesc">Dont use FaceBook its very useless.</span>
         </div>
         <div className="loginRight">
-            <form className="loginBox" onSubmit={handleClick}>
-                <input placeholder="Email" type="email" required className="loginInput" ref={email}/>
-                <input placeholder="Password" type="password" minLength='6' required className="loginInput" ref={password}/>
-                <button className="loginButton">Login</button>
-                <span className="loginForgot">Forgot Password ?</span>
-                <button className="loginRegisterButton">Create a New Account</button>
-            </form>
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Email"
+              type="email"
+              required
+              className="loginInput"
+              ref={email}
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              minLength="6"
+              required
+              className="loginInput"
+              ref={password}
+            />
+            <button className="loginButton" type="submit" disabled={isFetching}>
+              {isFetching ? <CircularProgress color="white" size="20px"/> : "Login"}</button> 
+            <span className="loginForgot">Forgot Password ?</span>
+            <button className="loginRegisterButton">
+            {isFetching ? <CircularProgress color="white" size="20px"/> : "Create a New Account"}
+            </button>
+          </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Login;
