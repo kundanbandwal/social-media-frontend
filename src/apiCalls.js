8aa4c6b1cdabc0658a609549a2f85bffdb895d5e
kundan <kundanbandwal@gmail.com>
2022-09-21 18:07:22 +0530

    update apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,9 @@
 import request from "./axiosConfig"
 
-export const loginCall = async (userCredentials, dispatch) => {
+ export const loginCall = async (userCredential, dispatch) => {
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await request.post("/auth/login", userCredentials);
+      const res = await request.post("/auth/login", userCredential);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err });
