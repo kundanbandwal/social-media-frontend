58321765941b563f078f173ed9859889986c993f
kundan <kundanbandwal@gmail.com>
2022-09-21 10:36:17 +0530

    Update CloseFriend.js

--- src/components/closeFriend/CloseFriend.js
+++ src/components/closeFriend/CloseFriend.js
@@ -1,15 +1,15 @@
 import './closeFriend.css'
 
-function CloseFriend(props) {
+function CloseFriend({user}) {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
     return (
         <div>
             <li className="sidebarFriend">
-                <img className="sidebarFriendImage" src={PF+props.user.profilePicture} alt="" />
-                <span className="sidebarFriendName">{props.user.username}</span>
+                <img className="sidebarFriendImage" src={PF+user.profilePicture} alt="" />
+                <span className="sidebarFriendName">{user.username}</span>
             </li>
         </div>
     )
 }
 
-export default CloseFriend
+export default CloseFriend;
