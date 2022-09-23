6714273899fd78b5e15297a7f109020b357cb107
kundan <kundanbandwal@gmail.com>
2022-09-23 14:45:38 +0530

    Update Feed.js

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -15,7 +15,9 @@ function Feed({username}) {
         const res = username ? await request.get('/posts/profile/' + username) : 
         await request.get('/posts/timeline/' + user._id) ;
         // console.log(res)
-        setPosts(res.data)
+        setPosts(res.data.sort((p1,p2)=>{
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        }))
       } catch (e) {
         console.log(e);
       }
