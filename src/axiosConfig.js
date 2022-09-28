a9afd222ca0b1773f8647a046b7870f3dd8685fb
kundan <kundanbandwal@gmail.com>
2022-09-28 14:50:52 +0530

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Next we make an 'instance' of it
 const instance = axios.create({
 // .. where we make our configurations
-    baseURL: 'http://192.168.1.2:8800/api'
+    baseURL: 'http://192.168.1.12:8800/api'
 });
 
 // Where you would set stuff like your 'Authorization' header, etc ...
