7fe0363118d8334816239a80b15148356c9a2590
kundan <kundanbandwal@gmail.com>
2022-09-28 14:52:34 +0530

    Update Messenger.js

--- src/pages/messenger/Messenger.js
+++ src/pages/messenger/Messenger.js
@@ -3,8 +3,30 @@ import Topbar from "../../components/topbar/Topbar";
 import Conversation from "../../components/conversations/Conversation";
 import Message from "../../components/message/Message";
 import ChatOnline from "../../components/chatOnline/ChatOnline";
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import request from "../../axiosConfig";
 
 function Messenger() {
+  const [conversations, setConversations] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await request.get("/conversations/" + user._id);
+        // console.log(res)
+        setConversations(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getConversations();
+  }, [user._id]);
+   console.log(currentChat)
   return (
     <>
       <Topbar />
@@ -12,33 +34,40 @@ function Messenger() {
         <div className="chatMenu">
           <div className="chatMenuWrapper">
             <input className="chatMenuInput" placeholder="Search For Friends" />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {conversations.map((c) => (
+              <div onClick={()=>setCurrentChat(c)}>
+              <Conversation conversation={c} currentUser={user} />
+              </div>
+            ))}
           </div>
         </div>
         <div className="chatBox">
           <div className="chatBoxWrapper">
-            <div className="chatBoxTop">
-              <Message />
-              <Message own={true} />
-              <Message />
-              <Message />
-              <Message own={true} />
-              <Message />
-              <Message />
-              <Message />
-            </div>
-            <div className="chatBoxBottom">
-              <textarea
-                className="chatMessageInput"
-                placeholder="Aa"
-              ></textarea>
-              <button className="chatSubmitButton">Send</button>
-            </div>
+            {currentChat ? (
+              <>
+                <div className="chatBoxTop">
+                  <Message />
+                  <Message own={true} />
+                  <Message />
+                  <Message />
+                  <Message own={true} />
+                  <Message />
+                  <Message />
+                  <Message />
+                </div>
+                <div className="chatBoxBottom">
+                  <textarea
+                    className="chatMessageInput"
+                    placeholder="Aa"
+                  ></textarea>
+                  <button className="chatSubmitButton">Send</button>
+                </div>
+              </>
+            ) : (
+              <span className="noConversationText">
+                open conversation to start a chat !
+              </span>
+            )}
           </div>
         </div>
         <div className="chatOnline">
