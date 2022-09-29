e31342a4227c2ba2aba4e2ecd00448bab1b1ad97
kundan <kundanbandwal@gmail.com>
2022-09-29 19:34:58 +0530

    Create MessangerService.js

---
+++ src/services/MessangerService.js
@@ -0,0 +1,18 @@
+import request from "../axiosConfig"
+
+export const fetchPosts = async (user,username, setPosts) => {
+    try {
+      console.log('hi')
+      const res = username ? await request.get('/posts/profile/' + username) : 
+      await request.get('/posts/timeline/' + user._id) ;
+      setPosts(res.data.sort((p1,p2)=>{
+        return new Date(p2.createdAt) - new Date(p1.createdAt);
+      }))
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+export default {
+    fetchPosts
+}
