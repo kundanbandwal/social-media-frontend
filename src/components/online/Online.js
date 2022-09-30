eae0401bb3a9ed738dea4e6b56ca8a1349c8df45
kundan <kundanbandwal@gmail.com>
2022-09-30 23:26:51 +0530

    Update Online.js

--- src/components/online/Online.js
+++ src/components/online/Online.js
@@ -1,20 +1,25 @@
-import "./online.css"
+import "./online.css";
 
-function Online({user}) {
+function Online({ user }) {
+  console.log({ user });
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   // console.log({PF})
 
   return (
     <div>
-       <li className="rightbarFriend">
-            <div className="rightbarProfileImageContainer">
-                <img className="rightbarProfileImage" src={PF+user.profilePicture} alt=""  />
-              <span className="rightbarOnline"></span>
-            </div>
-            <span className="rightbarUsername">{user.username}</span>
-          </li>
+      <li className="rightbarFriend">
+        <div className="rightbarProfileImageContainer">
+          <img
+            className="rightbarProfileImage"
+            src={PF + user.profilePicture}
+            alt=""
+          />
+          <span className="rightbarOnline"></span>
+        </div>
+        <span className="rightbarUsername">{user.username}</span>
+      </li>
     </div>
-  )
+  );
 }
 
-export default Online
+export default Online;
