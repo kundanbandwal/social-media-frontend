eecb9b6491b45289b697275291876afe678c90ce
kundan <kundanbandwal@gmail.com>
2022-12-02 21:36:53 +0530

    User logged out

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,15 @@
-import request from "./axiosConfig"
+import request from "./axiosConfig";
 
- export const loginCall = async (userCredential, dispatch) => {
-    dispatch({ type: "LOGIN_START" });
-    try {
-      const res = await request.post("/auth/login", userCredential);
-      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
-    } catch (err) {
-      dispatch({ type: "LOGIN_FAILURE", payload: err });
-    }
-  };
-  
-  export const loggedOutCall = async (dispatch)=>{
-    dispatch({ type: "LOGIN_SUCCESS", payload: null });
+export const loginCall = async (userCredential, dispatch) => {
+  dispatch({ type: "LOGIN_START" });
+  try {
+    const res = await request.post("/auth/login", userCredential);
+    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+  } catch (err) {
+    dispatch({ type: "LOGIN_FAILURE", payload: err });
   }
+};
+
+export const loggedOutCall = async (dispatch) => {
+  dispatch({ type: "LOGIN_SUCCESS", payload: null });
+};
